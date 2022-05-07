eaa48960c8edfed3eb3f81da850055d23aae1d86
Ziyaad <Zpetersen@Bainscorp.com>
2022-05-07 11:14:04 +0200

    comment to explain

--- app/src/main/java/za/ac/cput_cafeteriaapp/Homepage.java
+++ app/src/main/java/za/ac/cput_cafeteriaapp/Homepage.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Homepage extends AppCompatActivity {
-
+    //comment to explain
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
