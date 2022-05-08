ac50e72ef335850e2f1691605da015e96fd604f9
Raeece <raeecesamuels100@gmail.com>
2022-05-08 12:50:08 +0200

    ok

---
+++ app/src/main/java/za/ac/cput_cafeteriaapp/UpdateAccount.java
@@ -0,0 +1,14 @@
+package za.ac.cput_cafeteriaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_account);
+    }
+}
