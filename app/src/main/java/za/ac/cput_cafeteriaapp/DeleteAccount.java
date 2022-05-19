83abe1e724c55f5cdb26afe22808883a0825043a
Raeece <raeecesamuels100@gmail.com>
2022-05-19 23:48:19 +0200

    Update DeleteAccount.java

--- app/src/main/java/za/ac/cput_cafeteriaapp/DeleteAccount.java
+++ app/src/main/java/za/ac/cput_cafeteriaapp/DeleteAccount.java
@@ -2,13 +2,39 @@ package za.ac.cput_cafeteriaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DeleteAccount extends AppCompatActivity {
+    private TextView txtLinkDelete;
+    private TextView txtLinkCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_account);
+
+        txtLinkDelete = findViewById(R.id.finalDelete);
+        txtLinkCancel = findViewById(R.id.cancelDeletion);
+
+        txtLinkDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent delete = new Intent(DeleteAccount.this,LoginPage.class);
+                startActivity(delete);
+            }
+        });
+
+        txtLinkCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cancel = new Intent(DeleteAccount.this,UserSettings.class);
+                startActivity(cancel);
+            }
+        });
     }
 }
