036e642e15647a4a6ffb4926570e48f009f73710
Ongezwa Gwaza <211272183@mycput.ac.za>
2022-08-18 22:42:18 +0200

    link order takeaway

--- app/src/main/java/za/ac/cput_cafeteriaapp/Homepage.java
+++ app/src/main/java/za/ac/cput_cafeteriaapp/Homepage.java
@@ -2,7 +2,9 @@ package za.ac.cput_cafeteriaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends AppCompatActivity {
     //comment to explain
@@ -10,5 +12,10 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void order(View view) {
+        Intent intent = new Intent(getApplicationContext(), OrderTakeAway.class);
+        startActivity(intent);
     }
 }
