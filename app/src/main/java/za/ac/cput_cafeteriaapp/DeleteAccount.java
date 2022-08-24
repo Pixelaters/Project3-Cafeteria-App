32339465a1eec8f8f9976d3728069e412f5c67fe
Raeece Samuels <81654846+Pain600@users.noreply.github.com>
2022-08-24 11:25:31 +0200

    Update DeleteAccount.java

--- app/src/main/java/za/ac/cput_cafeteriaapp/DeleteAccount.java
+++ app/src/main/java/za/ac/cput_cafeteriaapp/DeleteAccount.java
@@ -33,5 +33,13 @@ public class DeleteAccount extends AppCompatActivity {
         btnDeleteAccount = findViewById(R.id.finalDelete);
         btnCancel = findViewById(R.id.cancelDeletion);
 
+        btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            cursor = db.rawQuery("DELETE * FROM" + RegistrationDB.TABLE_1_NAME + "WHERE" +
+                    RegistrationDB.USER_NAME "=?");
+            }
+        });
+
     }
 }
