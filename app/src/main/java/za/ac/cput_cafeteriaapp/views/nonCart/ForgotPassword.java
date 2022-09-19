e681cd4a9deb26f82a1069103d1d84bab0d02102
Ziyaad <Zpetersen@Bainscorp.com>
2022-09-19 17:19:20 +0200

    User registration+Login+ForgotPassword

--- app/src/main/java/za/ac/cput_cafeteriaapp/views/nonCart/ForgotPassword.java
+++ app/src/main/java/za/ac/cput_cafeteriaapp/views/nonCart/ForgotPassword.java
@@ -13,12 +13,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import za.ac.cput_cafeteriaapp.DatabaseHelper.RegistrationDB;
 import za.ac.cput_cafeteriaapp.R;
@@ -26,168 +34,60 @@ import za.ac.cput_cafeteriaapp.R;
 public class ForgotPassword extends AppCompatActivity {
 
     //variables
-    private ProgressDialog progressDialog;
-    EditText etUserMail, etPassword, etConfirmPassword;
-    Button btnChangePassword;
-    TextView txtBack, txtSignIn;
-    SQLiteDatabase db;
-    SQLiteOpenHelper openHelper;
+    private EditText emailEditText;
+    private Button resetPasswordBtn;
+    private ProgressBar progressBar;
+
+    FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        // initialise Db
-        openHelper = new RegistrationDB(this);
-        db = openHelper.getReadableDatabase();
 
-        //initialise progress progress dialog
-        progressDialog = new ProgressDialog(this, ProgressDialog.THEME_HOLO_LIGHT);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setCancelable(false);
+        emailEditText= (EditText) findViewById(R.id.emailForgotPassword);
+        resetPasswordBtn=(Button) findViewById(R.id.ResetPasswordForgotPassword);
+        progressBar= (ProgressBar) findViewById(R.id.progressBar6);
 
+        auth= FirebaseAuth.getInstance();
 
-        //link variables with xml views
-        etUserMail = findViewById(R.id.userMail);
-        etPassword = findViewById(R.id.password);
-        etConfirmPassword = findViewById(R.id.confirm_password);
-        btnChangePassword = findViewById(R.id.change_password_btn);
-        txtBack = findViewById(R.id.back);
-        txtSignIn = findViewById(R.id.sign_in);
-
-        btnChangePassword.setOnClickListener(view -> {
-            /* check if user inputs are valid and display
-            corresponding errors
-             */
-            if (!isValid() || !userExists()) {
-                return;
+        resetPasswordBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                resetPassword();
             }
-            progressDialog.setMessage("Changing password...");
-            showDialog();
-            changePassword();
-
         });
 
-        txtBack.setOnClickListener(view -> {
-           // close this activity and return to previous activity
-            finish();
-        });
-
-        txtSignIn.setOnClickListener(view -> {
-           // go to login page
-            login();
-        });
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        db.close();
-    }
-
-    private void showDialog() {
-        // display progress dialog if not already shown
-        if (!progressDialog.isShowing())
-            progressDialog.show();
-    }
-
-    private void hideDialog() {
-        //hide the progress dialog if it is already showing
-        if (progressDialog.isShowing()) {
-            progressDialog.dismiss();
-        }
-    }
-
-
-    public boolean isValid() {
-        boolean valid;
-
-        String userMail = etUserMail.getText().toString();
-        String password = etPassword.getText().toString();
-        String confirmPassword = etConfirmPassword.getText().toString();
-
-        // validate username / email field
-        if (userMail.isEmpty()) {
-            etUserMail.setError("Username / email must not be empty");
-            valid = false;
-        } else if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            etPassword.setError("Password should be greater than 4 alphanumeric characters");
-            valid = false;
-        } else if (confirmPassword.isEmpty() || confirmPassword.length() < 4 || confirmPassword.length() > 10) {
-            // set minimum requirements for password
-            etConfirmPassword.setError("Password should be greater than 4 alphanumeric characters");
-            valid = false;
-        } else if (!confirmPassword.equals(password)) {
-            // check if passwords match
-            etConfirmPassword.setError("Passwords must match");
-            valid = false;
-        } else {
-            etUserMail.setError(null);
-            etPassword.setError(null);
-            etConfirmPassword.setError(null);
-            valid = true;
         }
 
-        return valid;
-    }
+    private void resetPassword(){
+        String email= emailEditText.getText().toString().trim();
 
-    public boolean userExists() {
-        boolean userExists;
-        String userMail = etUserMail.getText().toString();
-        // check if user exists in db
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_1_NAME + " WHERE " +
-                RegistrationDB.USER_NAME + "=? OR " + RegistrationDB.EMAIL + "=?", new String[]{userMail, userMail});
-
-        if (cursor != null && cursor.getCount() > 0) {
-            userExists = true;
-        } else {
-            Toast toast = Toast.makeText(getApplicationContext(), "User does not exist.Register Instead or Check the username /email and try again ", Toast.LENGTH_LONG);
-            toast.show();
-            userExists = false;
+        if (email.isEmpty()){
+            emailEditText.setError("Username is required");
+            emailEditText.requestFocus();
+            return;
         }
-        return userExists;
-    }
 
-    // code to change the password
-    public void changePassword() {
-        openHelper = new RegistrationDB(this);
-        SQLiteDatabase db = openHelper.getReadableDatabase();
-        String password = etPassword.getText().toString();
-        String userMail = etUserMail.getText().toString();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Please provide a valid email");
+            emailEditText.requestFocus();
+            return;
+        }
 
-        ContentValues values = new ContentValues();
-        values.put(PASSWORD, password);
-        try {
-            int result = db.update(TABLE_1_NAME, values, USER_NAME + " = ? OR " + EMAIL + " = ?",
-                    new String[]{userMail, userMail});
-
-            if (result == 1) {
-                new Handler().postDelayed(() -> {
-                    hideDialog();
-                    //  display toast message
-                    Toast.makeText(getApplicationContext(), "Password Changed Successfully. Please login with new password", Toast.LENGTH_LONG).show();
-                    login();
-                }, 5000);
-
-            } else {
-                hideDialog();
-                Toast.makeText(getApplicationContext(), "Failed to change password", Toast.LENGTH_LONG).show();
+        progressBar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(ForgotPassword.this, "Check your email to reset your password!", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }else{
+                    Toast.makeText(ForgotPassword.this, "Try again! Something wrong happened!", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
             }
-
-        } catch (Exception ex) {
-            hideDialog();
-            Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
-        }finally {
-            db.close();
-        }
-
-    }
-
-    private void login() {
-        //            go to login page
-        Intent intent = new Intent(getApplicationContext(), LoginPage.class);
-        startActivity(intent);
+        });
     }
 }
